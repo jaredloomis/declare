bac87da20952f1ddc7af4132f06998ae43473d03
Jared Loomis <jaredloomis@users.noreply.github.com>
2018-10-15 15:11:13 -0700

    Add test code for Vultr

--- server/src/schema/query/Account.js
+++ server/src/schema/query/Account.js
@@ -7,6 +7,9 @@ import CanError from "../GraphQLCanError"
 import {Account as AccountModel}  from "declare-db"
 import AccountAccess from "../../access/Account"
 
+console.log("AccountModel", AccountModel)
+console.log("gql", AccountModel.graphQL)
+
 export default {
     accounts: {
         type: CanError(new GraphQLList(AccountModel.graphQL), {name: "List_Account_CanError"}),
