115d2bc15094906a329d09372ce07ceb0b080c42
Jared Loomis <jaredloomis@users.noreply.github.com>
2018-10-14 22:30:00 -0700

    ADd index file for Lambda

---
+++ executor/index.js
@@ -0,0 +1,62 @@
+// @flow
+import {config}         from "declare-common"
+import {Page, TestPack} from "declare-db"
+import Runner           from "./Runner"
+import Report           from "./Report"
+import SeleniumDriver   from "./SeleniumDriver"
+import {executeCustomTest} from "./CustomTest"
+import executeTestRun   from "./TestRun"
+import screenshot       from "./modules/screenshot"
+import destructive      from "./modules/destructive-input"
+const {internalIDs} = config.TestPack
+
+export type Module = (runner: Runner, page: Page, values: any, data: any) => any
+
+export {
+    executeCustomTest, Report, executeTestRun
+}
+
+export const lambdaHandler = async (event, context) => {
+    return JSON.stringify({event, context})
+}
+
+export const executePack = async (pageID: string, packID: string) => {
+    // Collect data for module
+    const {internalID} = await TestPack.findOne({_id: packID})
+    const page   = await Page.findOne({_id: pageID})
+    const tpData = page.testPackData
+    const datum  = tpData && tpData.filter(dat =>
+        dat.testPack.toString() === packID
+    )[0] || {}
+
+    // Create Runner
+    const driver = new SeleniumDriver({})
+    const runner = new Runner(driver, "Screenshot", {})
+    await runner.initNavigator()
+
+    // Find module
+    const module = findModule(internalID)
+
+    // Run module
+    if(module) {
+        const result = await module(runner, page, datum.values, datum.data)
+        await runner.quit()
+        return processResult(result, pageID, packID)
+    } else {
+        return {}
+    }
+}
+
+function findModule(packID: string): ?Module {
+    return packID === internalIDs.screenshot  ? screenshot  :
+           packID === internalIDs.destructive ? destructive :
+                                                null
+}
+
+const processResult = (result, pageID, packID) => ({
+    ...result,
+    report: {
+        ...result.report,
+        pageID, packID
+    }
+})
