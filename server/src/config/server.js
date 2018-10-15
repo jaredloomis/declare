8a89f984f289e941209ed7490c1e7bc1e588c31f
Jared Loomis <jaredloomis@users.noreply.github.com>
2018-10-15 14:34:28 -0700

    Add missing js files

---
+++ server/src/config/server.js
@@ -0,0 +1,11 @@
+export const production = {
+    port: process.env.PORT || 80
+}
+
+export const development = {
+    port: process.env.PORT || 3000
+}
+
+export default (process.env.NODE_ENV || "").indexOf("prod") === 0 ?
+    production :
+    development
