cf4efa53a3a128524568d4b85061f964e2f69c1c
Jared Loomis <jaredloomis@users.noreply.github.com>
2018-11-28 00:36:22 -0800

    Restore accidentally deleted file

---
+++ client/js/graphQL/Client.js
@@ -0,0 +1,56 @@
+import {ApolloClient}  from "apollo-client"
+import {HttpLink}      from "apollo-link-http"
+import {WebSocketLink} from "apollo-link-ws"
+import {setContext}    from "apollo-link-context"
+import {InMemoryCache} from "apollo-cache-inmemory"
+
+const cachedClients = {}
+
+export default (token, options={}) => {
+    // Create cookie
+    document.cookie = `declare_token=${token}`
+
+    // Create links
+    const authLink = setContext((_, {headers}) => ({
+        headers: {
+            ...headers,
+            token
+        }
+    }))
+    const netLink = (() => {
+        if(options.webSocket) {
+            return new WebSocketLink({
+                uri: `ws://localhost:3000`,
+                options: {
+                    reconnect: true
+                }
+            })
+        } else {
+            return new HttpLink({uri: "/graphql"})
+        }
+    })()
+
+    // Check if a valid client is already cached
+    if(cachedClients[token]) {
+        const cached = cachedClients[token]
+
+        if(!!cached.options.webSocket === !!cached.options.webSocket) {
+            return cached.client
+        }
+    }
+
+    // Create client
+    const client = new ApolloClient({
+        link: authLink.concat(netLink),
+        cache: new InMemoryCache()
+    })
+
+    // Cache client
+    Object.assign(cachedClients, {
+        [token]: {
+            client, options
+        }
+    })
+
+    return client
+}
